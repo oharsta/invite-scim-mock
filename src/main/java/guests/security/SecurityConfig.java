598f20cb2e57a6aab950e6935ae99c607cfd01d1
Okke Harsta <oharsta@zilverline.com>
2022-01-08 11:06:41 +0100

    BSR

--- src/main/java/guests/security/SecurityConfig.java
+++ src/main/java/guests/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package guests.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,6 +16,7 @@ public class SecurityConfig {
 
         private @Value("${inviter.user}")
         String user;
+
         private @Value("${inviter.password}")
         String password;
 
@@ -39,12 +39,11 @@ public class SecurityConfig {
 
         @Override
         protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder> builder = auth
-                    .inMemoryAuthentication()
+            auth.inMemoryAuthentication()
                     .withUser(user)
                     .password("{noop}" + password)
-                    .roles("openid")
-                    .and();
+                    .roles("openid");
+
 
         }
     }
